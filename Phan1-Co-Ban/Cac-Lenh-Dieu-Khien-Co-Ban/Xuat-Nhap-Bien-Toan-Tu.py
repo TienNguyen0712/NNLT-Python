99276630b4175902389b7f259c7c3ed7e4f0d6f2
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-10-24 23:35:51 +0700

    Add files via upload

---
+++ Phan1-Co-Ban/Cac-Lenh-Dieu-Khien-Co-Ban/Xuat-Nhap-Bien-Toan-Tu.py
@@ -0,0 +1,24 @@
+# Su dung ham print
+# BAI TAP 1
+# CAU A
+print("\n * * * * * \n * * * * * \n * * * * * \n * * * * * ")
+# CAU B
+print("\n * * * * * \n *       * \n * * * * * ")
+# CAU C
+print("\n   *  *  \n *       * \n *       * \n *       * \n   *  *  ")
+# CAU D
+print("\n * * * * * \n   * * *   \n   * * *   \n   * * *   \n * * * * * ")
+# BAI TAP 2
+print("xxxxx  xx  xx  xxxxxx xx  xx   xxxx    xx  xx")
+print("xx  xx xx  xx    xx   xx  xx  xx  xx   xxx xx")
+print("xxxxx  xx  xx    xx   xxxxxx  xx  xx   xxxxxx")
+print("xx       xx      xx   xx  xx  xx  xx   xx xxx")
+print("xx       xx      xx   xx  xx   xxxx    xx  xx")
+# BAI TAP 3
+print("QUÊ HƯƠNG")
+print(". . .")
+print("Quê hương là chùm khế ngọt")
+print("    Cho con trèo hái mỗi ngày")
+print("        Quê hương là đường đi học")
+print("            Con về rợp bướm vàng bay \n")
+print("                         Giáp Văn Thạch")

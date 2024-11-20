e4b2b9eb0cbba13b6680e759410fef21f38e8f24
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-11-20 07:36:06 +0700

    Update Xuat-Nhap-Bien-Toan-Tu.py

--- Phan1-Co-Ban/Cac-Lenh-Dieu-Khien-Co-Ban/Xuat-Nhap-Bien-Toan-Tu.py
+++ Phan1-Co-Ban/Cac-Lenh-Dieu-Khien-Co-Ban/Xuat-Nhap-Bien-Toan-Tu.py
@@ -678,3 +678,23 @@ def main():
 
 if __name__ == "__main__":
     main()
+# BAI TAP 45
+def sont(a):
+    c = 0
+    for i in range(1, a+1):
+        if a % i == 0:
+            c+=1
+    if c == 2:
+        return True
+    else:
+        return False
+
+def main():
+    n = int(input("Nhap n: "))
+    for i in range(1, n + 1):
+        if n % i == 0:
+            if sont(i):
+                print(i, end=" ")
+
+if __name__ == "__main__":
+    main()
